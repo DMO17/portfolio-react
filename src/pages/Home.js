aba5c9420ef836bed556f3a8d8308fd169125f66
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-25 22:44:05 +0000

    profile banner done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { ProfileBanner } from "../components/ProfileBanner";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <ProfileBanner />
+    </>
+  );
 };
