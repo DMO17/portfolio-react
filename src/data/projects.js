6d834bc6892d73f77383a79f6f9859a84e9c2290
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-28 12:57:36 +0000

    created project data

---
+++ src/data/projects.js
@@ -0,0 +1,161 @@
+const projectTypes = {
+  fullStack: "Full-stack",
+  backEnd: "Back-end",
+  frontEnd: "Front-end",
+  react: "React",
+  htmlCssJs: "html/css/js",
+};
+
+const projectInfo = [
+  {
+    title: "Food4U ",
+    projectImg:
+      "https://github.com/DMO17/food4U/raw/main/public/assets/images/food-logo.png",
+    githubLink: "https://github.com/DMO17/food4U",
+    liveDemoLink: "https://food-4u.herokuapp.com/",
+    projectType: projectTypes.fullStack,
+  },
+  {
+    title: "Model-View-Controller-Tech-Blog",
+    projectImg:
+      "https://github.com/DMO17/portfolio/blob/main/assets/screenshots/tech-blog.png?raw=true",
+    githubLink: "https://github.com/DMO17/Model-View-Controller-Tech-Blog",
+    liveDemoLink: "https://tech-blogs-m-v-c.herokuapp.com/",
+    projectType: projectTypes.fullStack,
+  },
+  {
+    title: "E-commerce-back-end-orm",
+    projectImg:
+      "https://github.com/DMO17/portfolio/blob/main/assets/screenshots/e-commerce.png?raw=true",
+    githubLink:
+      "https://github.com/DMO17/e-commerce-back-end-orm/blob/main/README.md",
+    liveDemoLink:
+      "https://drive.google.com/file/d/1olYhwqL7l6MGR6DnwRq9S2GOkhSUsGPe/view",
+    projectType: projectTypes.backEnd,
+  },
+  {
+    title: "Pokedecks",
+    projectImg:
+      "https://media.istockphoto.com/vectors/seamless-question-mark-asking-questions-quiz-background-pattern-vector-id1335403960?s=612x612",
+    githubLink: "https://github.com/DMO17/food4U",
+    liveDemoLink: "https://food-4u.herokuapp.com/",
+    projectType: projectTypes.fullStack,
+  },
+  {
+    title: "Flash Cards",
+    projectImg:
+      "https://media.istockphoto.com/vectors/seamless-question-mark-asking-questions-quiz-background-pattern-vector-id1335403960?s=612x612",
+    githubLink: "https://github.com/DMO17/food4U",
+    liveDemoLink: "https://food-4u.herokuapp.com/",
+    projectType: projectTypes.react,
+  },
+  {
+    title: "Finance Price Tracker",
+    projectImg:
+      "https://media.istockphoto.com/vectors/seamless-question-mark-asking-questions-quiz-background-pattern-vector-id1335403960?s=612x612",
+    githubLink: "https://github.com/DMO17/food4U",
+    liveDemoLink: "https://food-4u.herokuapp.com/",
+    projectType: projectTypes.react,
+  },
+  {
+    title: "Travel Log",
+    projectImg:
+      "https://media.istockphoto.com/vectors/seamless-question-mark-asking-questions-quiz-background-pattern-vector-id1335403960?s=612x612",
+    githubLink: "https://github.com/DMO17/food4U",
+    liveDemoLink: "https://food-4u.herokuapp.com/",
+    projectType: projectTypes.fullStack,
+  },
+  {
+    title: "NoSQL-Social-Network-API",
+    projectImg:
+      "https://media.istockphoto.com/photos/working-picture-id172175546?s=612x612",
+    githubLink: "https://github.com/DMO17/NoSQL-Social-Network-API/tree/dev",
+    liveDemoLink:
+      "https://drive.google.com/file/d/1BTWENeaGzrTyUB1rPJmjQ-JwOfu77WFQ/view",
+    projectType: projectTypes.backEnd,
+  },
+  {
+    title: "Employee-Tracker",
+    projectImg:
+      "https://github.com/DMO17/employee-tracker/raw/main/screenshots/app-preview-2.PNG",
+    githubLink: "https://github.com/DMO17/employee-tracker",
+    liveDemoLink:
+      "https://drive.google.com/file/d/1T8juFqB1MvFMToUagMeRDp75ay46f5Pt/view",
+    projectType: projectTypes.backEnd,
+  },
+  {
+    title: "NOTE-TAKER",
+    projectImg:
+      "https://github.com/DMO17/employee-tracker/raw/main/screenshots/app-preview-2.PNG",
+    githubLink: "https://github.com/DMO17/note-taker/tree/dev",
+    liveDemoLink: "https://murmuring-basin-33128.herokuapp.com/notes",
+    projectType: projectTypes.fullStack,
+  },
+  {
+    title: "Team-Profile-Generator",
+    projectImg:
+      "https://github.com/DMO17/team-profile-generator/raw/dev/dist/assets/images/code-preview.PNG",
+    githubLink: "https://github.com/DMO17/team-profile-generator",
+    liveDemoLink:
+      "https://drive.google.com/file/d/1wB6xwaIL2Ra1J6YIZxavdqfMgsExmtCN/view",
+    projectType: projectTypes.backEnd,
+  },
+  {
+    title: "ReadMe-generator",
+    projectImg:
+      "https://github.com/DMO17/readme-generator/blob/main/assets/screenshots/code-preview.PNG?raw=true",
+    githubLink: "https://github.com/DMO17/readme-generator",
+    liveDemoLink:
+      "https://drive.google.com/file/d/1_Y32uh2hKJETCJzRm4CNQ0r33kZ36Y9M/view?usp=sharing",
+    projectType: projectTypes.backEnd,
+  },
+  {
+    title: "Sound Art Smart",
+    projectImg:
+      "https://github.com/DMO17/portfolio/blob/main/assets/screenshots/sound-art-smart.PNG?raw=true",
+    githubLink: "https://github.com/DMO17/sound-art-smart/tree/main",
+    liveDemoLink: "https://gingus55.github.io/sound-art-smart/",
+    projectType: projectTypes.htmlCssJs,
+  },
+  {
+    title: "Weather Dashboard",
+    projectImg:
+      "https://github.com/DMO17/weather-dashboard/raw/main/assets/screenshots/web-preview-2.PNG",
+    githubLink: "https://github.com/DMO17/weather-dashboard",
+    liveDemoLink: "https://dmo17.github.io/weather-dashboard/",
+    projectType: projectTypes.htmlCssJs,
+  },
+  {
+    title: "Work Day Planner",
+    projectImg:
+      "https://github.com/DMO17/portfolio/blob/main/assets/screenshots/work-day.PNG?raw=true",
+    githubLink: "https://github.com/DMO17/work-day-planner",
+    liveDemoLink: "https://dmo17.github.io/work-day-planner/",
+    projectType: projectTypes.htmlCssJs,
+  },
+  {
+    title: "Coding Quiz Challenge",
+    projectImg:
+      "https://github.com/DMO17/portfolio/blob/main/assets/screenshots/code-quiz.PNG?raw=true",
+    githubLink: "https://github.com/DMO17/coding-quiz-challenge",
+    liveDemoLink: "https://dmo17.github.io/coding-quiz-challenge//",
+    projectType: projectTypes.htmlCssJs,
+  },
+  {
+    title: "Random Password Generator Js",
+    projectImg:
+      "https://github.com/DMO17/portfolio/blob/main/assets/screenshots/password-generator.PNG?raw=true",
+    githubLink: "https://github.com/DMO17/random-password-generator-javascript",
+    liveDemoLink:
+      "https://dmo17.github.io/random-password-generator-javascript/",
+    projectType: projectTypes.htmlCssJs,
+  },
+  {
+    title: "Semantic Html and Css",
+    projectImg:
+      "https://github.com/DMO17/portfolio/blob/main/assets/images/web-page-preview.png?raw=true",
+    githubLink: "https://github.com/DMO17/semantic-html-refactor",
+    liveDemoLink: "https://dmo17.github.io/semantic-html-refactor/",
+    projectType: projectTypes.htmlCssJs,
+  },
+];
